3894abfe318869328aae9123998654f15f560f78
jackahl <kahl@kitconcept.com>
2018-11-22 14:51:05 +0100

    removed header

--- src/components/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = ({ siteTitle }) => (
-  <div>
-
-  </div>
-)
-
-export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import Header from './header'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -27,7 +26,6 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
         <div>
           {children}
         </div>
