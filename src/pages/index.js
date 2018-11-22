33a9ae39e4f597ff344e03bdab854440759adec3
jackahl <kahl@kitconcept.com>
2018-11-22 11:36:08 +0100

    added initial HTML

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,175 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <body>
+
+      <div class="above-the-fold">
+        <div class="container">
+          <h1>React Barcamp</h1>
+        </div>
+      </div>
+      <div class="container about">
+        <h1>About</h1>
+        <div class="row">
+          <div class="col col-12 col-sm-6">
+            <div class="info-text">
+              <h3>What is a Barcamp?</h3>
+              <p>A BarCamp is an event shaped by your contribution. Rather than having scheduled
+                  speakers,
+                  you
+                  will have the opportunity to talk about what is important to you. Anyone with something
+                  to
+                  contribute in the context of user experience is welcome to join. The goal is to share
+                  what
+                  you
+                  know, to learn from others, to ignite discussion, and to connect in an open
+                    environment.</p>
+            </div>
+          </div>
+          <div class="col col-6 d-none d-sm-block">
+            <div class="image">
+              <img class="info-picture" src="react-icon.png" alt="placeholder" />
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col col-6 d-none d-sm-block">
+            <div class="image">
+              <img class="info-picture" src="react-icon.png" alt="placeholder" />
+            </div>
+          </div>
+          <div class="col col-12 col-sm-6">
+            <div class="info-text">
+              <h3>What is React?</h3>
+              <p>React is a Javascript library that makes it incredible easy to
+                  build fast user
+                  Interfaces
+                  for Websites and mobile. It allows you to create reusable components to reduce
+                  repetitive
+                  programing and the overall amount of code. This makes not only writing but also
+                  reading
+                  the
+                    code much easier.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container schedule">
+        <h1>Schedule</h1>
+        <div class="row ">
+          <div class="col col-12 col-sm-6">
+            <div class="table">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sit eligendi ea porro cupiditate
+                  ex excepturi, at, doloremque accusamus quia ut repellat dicta, ab ipsum numquam! Quibusdam
+                    natus nihil minima.</p>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-6">
+            <div class="table">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit vel quas natus doloribus,
+                  veritatis architecto id excepturi exercitationem ad corrupti vitae veniam minima accusamus
+                    ducimus voluptates nisi enim ex deleniti.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container sponsors">
+        <h1>Sponsors</h1>
+        <div class="row">
+          <div class="col col-12 col-sm-6 col-md-3">
+            <div class="card">
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-6 col-md-3">
+            <div class="card">
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-6 col-md-3">
+            <div class="card">
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-6 col-md-3">
+            <div class="card">
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col col-12 col-sm-6 col-md-3">
+            <div class="card">
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-6 col-md-3">
+            <div class="card">
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-6 col-md-3">
+            <div class="card">
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-6 col-md-3">
+            <div class="card">
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid location">
+        <h1>Location</h1>
+        <div class="map">
+
+        </div>
+      </div>
+      <div class="container-fluid footer">
+        <div class="container">
+          <h1>Footer</h1>
+          <div class="row">
+            <div class="col col-12 col-sm-6 col-md-3">
+              <div class="footer-text">
+                <p>test</p>
+              </div>
+            </div>
+            <div class="col col-12 col-sm-6 col-md-3">
+              <div class="footer-text">
+                <p>test</p>
+              </div>
+            </div>
+            <div class="col col-12 col-sm-6 col-md-3">
+              <div class="footer-text">
+                <p>test</p>
+              </div>
+            </div>
+            <div class="col col-12 col-sm-6 col-md-3">
+              <div class="footer-text">
+                <p>test</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
   </Layout>
 )
 
