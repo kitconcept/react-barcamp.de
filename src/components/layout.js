24fa410ab30570b67cf8662936c4831d4accceed
jackahl <kahl@kitconcept.com>
2018-11-27 11:31:20 +0100

    moved bootstrap import to layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import 'bootstrap/dist/css/bootstrap.css'
 import './layout.css'
 
 const Layout = ({ children }) => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import 'bootstrap/dist/css/bootstrap.css'
 import BarcampSession from '../images/BarcampSession.jpg'
 import Programmer from '../images/programmer.jpg'
 import Reacticon from '../images/react-icon-sm.png'
